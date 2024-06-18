9537c6d8325de7619d50f51b00317acd00c754a4
wiriim <51820876+wiriim@users.noreply.github.com>
2024-06-18 18:14:04 +0700

    Update login.py

--- login.py
+++ login.py
@@ -30,6 +30,9 @@ if st.session_state["authentication_status"]:
 elif st.session_state["authentication_status"] == False:
     st.error('Username/Password is incorrect')
 elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')   
+    st.warning('Please enter your username and password') 
+        
+if st.button("Register"):
+    st.switch_page("pages/register.py")
 
 # streamlit run /Users/william/Documents/NoFoodWaste/login.py --client.showSidebarNavigation=False
